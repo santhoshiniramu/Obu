4a5af4d659b97e1e48015833544e4da3dc2ae02b
user <user@DESKTOP-2QL42S5>
2023-09-15 11:13:19 +0530

    Frist commit

---
+++ src/Kerithi.java
@@ -0,0 +1,26 @@
+
+public class Kerithi {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int m = 20 ;
+		int o = 30;
+		
+		if (n>m && n>o) {
+		      System.out.println("Good ");
+		    } 
+		
+		else if (m>n && m>o) {
+			System.out.println("average");
+			
+		}
+		else if (o>n && o>m)  {
+		      System.out.println("this is greatest number ");
+		    }  
+		    
+		    else  {
+			      System.out.println("invalid.");
+			    }  
+	}
+
+}
